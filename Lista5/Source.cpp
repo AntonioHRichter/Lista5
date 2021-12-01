ebbcff416d2f3fd19f0e3db5ea79e2c52fb5483a
Chockro <antoniohrichter@hotmail.com>
2021-12-01 19:36:09 -0300

    .

--- Lista5/Source.cpp
+++ Lista5/Source.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
     SceneManager* sceneManager = new SceneManager();
-    sceneManager->initialize(1200, 900, "Akeno MEGAMI <3", 
-        "../textures/Akeno-samaS2.jpg");
+    sceneManager->initialize(1200, 900, "CoolScene", 
+        "../textures/Ahh.jpg");
 
     sceneManager->run(Ponto3d(1,1,1));
 }
